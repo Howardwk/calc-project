75d1a07af3c330a00d8e2adf83583b4535567db3
Howardwk <159946962+Howardwk@users.noreply.github.com>
2024-02-18 20:33:52 +0300

    Add files via upload

---
+++ ammended calc.c
@@ -0,0 +1,46 @@
+//KAREMA HOWARD WANYEKI
+//ENE212-0071/2021
+#include <stdio.h>
+#include <math.h>
+
+
+int main() {
+    double num1, num2;
+    char operator;
+
+    printf("Enter the first number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter the operator: ");
+    scanf(" %c", &operator); 
+
+    printf("Enter the second number: ");
+    scanf("%lf", &num2);
+
+    switch (operator) {
+        case '+':
+            printf("Sum is: %lf", num1 + num2);
+            break;
+
+        case '-':
+            printf("Difference is: %lf", num1 - num2);
+            break;
+
+        case '/':
+            if (num2 != 0)
+                printf("Division is: %lf", num1 / num2);
+            else
+                printf("Error: Division by zero");
+            break;
+
+        case '*':
+            printf("Product is: %lf", num1 * num2);
+            break;
+
+
+        default:
+            printf("Invalid operator");
+            break;
+    }
+    return 0;
+}
